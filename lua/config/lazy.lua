bb6915db41f21340d04ed301ff611769111966ff
ysl2 <www.songli.yu@gmail.com>
2024-06-12 23:40:17 +0800

    Add extras.coding
    Disable codeium.nvim
    
    Finish.
    
    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    -- { import = "lazyvim.plugins.extras.coding" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -51,6 +52,6 @@ require("lazy").setup({
     },
   },
   git = {
-    url_format = 'git@github.com:%s.git',
-  }
+    url_format = "git@github.com:%s.git",
+  },
 })
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -7,27 +7,26 @@ return {
       news = {
         lazyvim = false,
       },
-    }
+    },
   },
   { "folke/noice.nvim", enabled = false },
   {
     "williamboman/mason.nvim",
     opts = {
       github = {
-        download_url_template = "git@github.com:%s/releases/download/%s/%s",
-      }
-    }
+        download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       local parsers = require("nvim-treesitter.parsers").get_parser_configs()
       for _, p in pairs(parsers) do
-        p.install_info.url = p.install_info.url:gsub(
-          "https://github.com/",
-          "https://mirror.ghproxy.com/https://github.com/"
-        )
+        p.install_info.url =
+          p.install_info.url:gsub("https://github.com/", "https://mirror.ghproxy.com/https://github.com/")
       end
-    end
+    end,
   },
+  { "Exafunction/codeium.nvim", enabled = false },
 }
