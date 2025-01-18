7780a6c4cc5586feb6741c2faf19797d1448205e
ysl2 <www.songli.yu@gmail.com>
2025-01-18 18:43:23 +0800

    Fix keymaps
    Fix
    
    Fix
    
    Fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,33 @@
 -- Add any additional keymaps here
 vim.keymap.del("n", "H")
 vim.keymap.del("n", "L")
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+
+vim.keymap.del("v", "<")
+vim.keymap.del("v", ">")
+
+vim.keymap.set("n", "<leader>ba", function() Snacks.bufdelete.all() end, { desc = "Delete All Buffers" })
+vim.keymap.set({ "i", "n", "s", "x" }, "<C-c>", "<esc>", { remap = true, desc = "Escape and Clear hlsearch" })
+vim.cmd([[
+	" start of line
+	:cnoremap <C-A>		<Home>
+	" back one character
+	:cnoremap <C-B>		<Left>
+	" delete character under cursor
+	:cnoremap <C-D>		<Del>
+	" end of line
+  :cnoremap <C-E>		<End>
+	" forward one character
+	:cnoremap <C-F>		<Right>
+	" recall newer command-line
+	:cnoremap <C-N>		<Down>
+	" recall previous (older) command-line
+	:cnoremap <C-P>		<Up>
+	" back one word
+	:cnoremap <Esc><C-B>	<S-Left>
+	" forward one word
+	:cnoremap <Esc><C-F>	<S-Right>
+]])
