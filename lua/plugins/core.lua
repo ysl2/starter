e4179a84419fc6f972c3852f06aa513df53c0a24
ysl2 <www.songli.yu@gmail.com>
2025-01-18 15:57:41 +0800

    bufferline remove close icon and change mode to tab.
    Fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -200,4 +200,14 @@ return {
       },
     }
   },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      }
+    }
+  },
 }
