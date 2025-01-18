8c97b130ae1b18973f7610100f0ca08d4a05a83c
ysl2 <www.songli.yu@gmail.com>
2025-01-18 14:26:03 +0800

    Add firenvim
    Fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -66,4 +66,34 @@ return {
       dashboard = { enabled = false },
     }
   },
+  {
+    "glacambre/firenvim",
+    build = function() vim.fn["firenvim#install"](0) end,
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    cond = not not vim.g.started_by_firenvim,
+    config = function()
+      vim.cmd("set guifont=FiraCode\\ Nerd\\ Font:h25")
+      vim.cmd("set laststatus=0")
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = { "github.com_*.txt", "gitee.com_*.txt" },
+        command = "set filetype=markdown"
+      })
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = { "leetcode.com_*.txt", "leetcode.cn_*.txt" },
+        command = "set filetype=python"
+      })
+      vim.g.firenvim_config = {
+        localSettings = {
+          -- ["https?://[^/]+\\.zhihu\\.com/*"] = { priority = 1, takeover = "never" },
+          -- ["https?://www\\.notion\\.so/*"] = { priority = 1, takeover = "never" },
+          -- ["https?://leetcode\\.com.*playground.*shared"] = { priority = 1, takeover = "never" },
+          -- ["https?://github1s\\.com/*"] = { priority = 1, takeover = "never" },
+          -- ["https?://docs\\.qq\\.com/*"] = { priority = 1, takeover = "never" },
+          -- [".*"] = { priority = 0 },
+          [".*"] = { priority = 1, takeover = "never" },
+        }
+      }
+    end
+  },
 }
