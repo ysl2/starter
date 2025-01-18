72a0be54a027c5ecd9df3b5949dedb37226d22d3
ysl2 <www.songli.yu@gmail.com>
2025-01-18 15:16:08 +0800

    Add persistence restore session
    Fix
    
    Fix
    
    Fix
    
    Fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -136,4 +136,36 @@ return {
       }
     }
   },
+  {
+    "folke/persistence.nvim",
+    lazy = false,
+    opts = function()
+      -- Auto restore session
+      vim.api.nvim_create_autocmd("VimEnter", {
+        nested = true,
+        callback = function()
+          local persistence = require("persistence")
+          if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+            persistence.load()
+          else
+            persistence.stop()
+          end
+        end,
+      })
+      -- Auto delete [No Name] buffers.
+      vim.api.nvim_create_autocmd("BufLeave", {
+        callback = function()
+          local buffers = vim.fn.filter(vim.fn.range(1, vim.fn.bufnr("$")),
+            'buflisted(v:val) && empty(bufname(v:val)) && bufwinnr(v:val) < 0 && (getbufline(v:val, 1, "$") == [""])')
+          local next = next
+          if next(buffers) == nil then return end
+          local cmdstr = ":silent! bw!"
+          for _, v in pairs(buffers) do
+            cmdstr = cmdstr .. " " .. v
+          end
+          vim.cmd(cmdstr)
+        end
+      })
+    end
+  },
 }
