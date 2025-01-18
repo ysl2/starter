9b82f8a1d63f3411db2c3630b3f0b7ca6a71ad6d
songliyu <songliyu@tencent.com>
2025-01-19 02:20:41 +0800

    Update lualine.nvim
    Fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -329,4 +329,13 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" }
+      },
+    }
+  }
 }
