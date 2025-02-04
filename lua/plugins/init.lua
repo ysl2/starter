b9e363b2df815d4a45b8afd096fbe4c11cd1cdc5
songliyu <songliyu@tencent.com>
2025-02-04 14:31:10 +0800

    Remap incremental selection.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -124,6 +124,15 @@ return {
     opts = {
       highlight = { additional_vim_regex_highlighting = { "python" } },
       indent = { disable = { "python" } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<cr>",
+          node_incremental = "<cr>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     },
   },
   {
