fffbec45b288d7a2cd7c402d71c47163e6b9fc7d
ysl2 <www.songli.yu@gmail.com>
2025-03-15 18:22:20 +0800

    Cancel the limitation of golang, the gopls has always been loaded by nvim-lspconfig. Fix "which" command for windows.
    Cancel the limitation of golang, because the gopls has been defined in
    nvim-lspconfig so that nvim have to load.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.lang.git" },
-    { import = "lazyvim.plugins.extras.lang.go", cond = vim.system({ "which", "go" }):wait().code == 0 },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
