cd71668cf4d71d68af86cb5c223b2eb3946f2891
ysl2 <www.songli.yu@gmail.com>
2025-03-15 19:24:16 +0800

    Keep explorer to neo-tree, and keep picker to fzf-lua.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
