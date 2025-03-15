958474dc19751672566a705a4c17fc14b0264acb
ysl2 <www.songli.yu@gmail.com>
2025-03-15 18:55:05 +0800

    Limit tmux only available on unix.
    Fix
    
    Fix
    
    Fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,47 +8,47 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local function clean_dangling_tmux_sessions(args)
-  local tmux = _G.localhost.MYTMUX or os.getenv("MYTMUX")
-  tmux = tmux ~= "" and tmux or "tmux"
+-- if vim.uv.os_uname().sysname:find("Windows") == nil then
+if not LazyVim.is_win() then
+  -- NOTE: Clean dangling tmux sessions
+  -- TODO: No effect on VimEnter, so disable it for now. Need to further investigate.
+  -- vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
+  vim.api.nvim_create_autocmd("VimLeave", {
+    callback = function(args)
+      local tmux = _G.localhost.MYTMUX or os.getenv("MYTMUX")
+      tmux = tmux ~= "" and tmux or "tmux"
 
-  -- Get all tmux sessions that have a name starting with "nvim-".
-  local stdout = vim.system({ tmux, "ls", "-F", '"#{session_name}"' }, { text = true }):wait().stdout
-  if not stdout then
-    return
-  end
-  local tmux_sessions = {}
-  for line in stdout:gmatch("[^\r\n]+") do
-    line = line:match('"(nvim%-.*)"')
-    table.insert(tmux_sessions, line)
-  end
+      -- Get all tmux sessions that have a name starting with "nvim-".
+      local stdout = vim.system({ tmux, "ls", "-F", '"#{session_name}"' }, { text = true }):wait().stdout
+      if not stdout then
+        return
+      end
+      local tmux_sessions = {}
+      for line in stdout:gmatch("[^\r\n]+") do
+        line = line:match('"(nvim%-.*)"')
+        table.insert(tmux_sessions, line)
+      end
 
-  -- If VimEnter and VimLeave, clear the tmux sessions that have no corresponding nvim process.
-  for _, line in ipairs(tmux_sessions) do
-    local pid = line:match("nvim%-([%w]+)")
-    local pname = vim.system({ "ps", "-p", pid, "-o", "args=" }, { text = true }):wait().stdout
-    if not pname or not pname:find("nvim%s+%-%-embed") then
-      vim.system({ tmux, "kill-session", "-t", line }, { text = true })
-    end
-  end
+      -- If VimEnter and VimLeave, clear the tmux sessions that have no corresponding nvim process.
+      for _, line in ipairs(tmux_sessions) do
+        local pid = line:match("nvim%-([%w]+)")
+        local pname = vim.system({ "ps", "-p", pid, "-o", "args=" }, { text = true }):wait().stdout
+        if not pname or not pname:find("nvim%s+%-%-embed") then
+          vim.system({ tmux, "kill-session", "-t", line }, { text = true })
+        end
+      end
 
-  -- If VimLeave, clear the tmux session that corresponds to the current nvim process.
-  if args.event == "VimLeave" then
-    local pid = vim.loop.os_getpid()
-    local pname = vim.system({ "ps", "-p", pid, "-o", "args=" }, { text = true }):wait().stdout
-    if pname and pname:find("nvim%s+%-%-embed") then
-      vim.system({ tmux, "kill-session", "-t", "nvim-" .. pid }, { text = true })
-    end
-  end
+      -- If VimLeave, clear the tmux session that corresponds to the current nvim process.
+      if args.event == "VimLeave" then
+        local pid = vim.loop.os_getpid()
+        local pname = vim.system({ "ps", "-p", pid, "-o", "args=" }, { text = true }):wait().stdout
+        if pname and pname:find("nvim%s+%-%-embed") then
+          vim.system({ tmux, "kill-session", "-t", "nvim-" .. pid }, { text = true })
+        end
+      end
+    end,
+  })
 end
-
--- TODO: No effect on VimEnter, so disable it for now. Need to further investigate.
--- vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function(args)
-    clean_dangling_tmux_sessions(args)
-  end,
-})
 
 -- Check if we need to reload the gitsigns when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
