9a2add53ca15af169855145a6e187ee28aa5ee27
ysl2 <songliyu97@gmail.com>
2025-06-21 23:52:04 +0800

    Move extras into lazyvim.json for `:LazyExtras` order.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,39 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-
-    -- Imports
-    -- NOTE: Priority ref: ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/xtras.lua
-    -- Priority: 10
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- Priority: 50 (default)
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    {
-      import = "lazyvim.plugins.extras.ai.copilot",
-      cond = not vim.g.started_by_firenvim and "leetcode.nvim" ~= vim.fn.argv(0, -1),
-    },
-    -- { import = "lazyvim.plugins.extras.ai.tabnine", cond = not vim.g.started_by_firenvim and "leetcode.nvim" ~= vim.fn.argv(0, -1) },
-    { import = "lazyvim.plugins.extras.coding.neogen" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.inc-rename" },
-    { import = "lazyvim.plugins.extras.editor.neo-tree" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.git" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.nushell" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.sql" },
-    { import = "lazyvim.plugins.extras.lang.tex" },
-    { import = "lazyvim.plugins.extras.lang.toml" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.ui.indent-blankline" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.util.project" },
-    -- Priority: 100
-    { import = "lazyvim.plugins.extras.editor.outline" },
-
     -- import/override with your plugins
     { import = "plugins" },
   },
